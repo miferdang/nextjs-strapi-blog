d577528f7919348ee8c109eb9dbf6caf7c7b8d33
miferdang <dangphuong19vn@gmail.com>
2024-06-30 11:38:48 +0700

    Change layout & Add comment to Article Page

--- frontend/app/articles/[id]/[slug]/page.tsx
+++ frontend/app/articles/[id]/[slug]/page.tsx
@@ -1,13 +1,17 @@
 "use client";
 
+import Comment from "@/app/_components/Comment/Comment";
+import CommentForm from "@/app/_components/Form/CommentForm";
 import RichText from "@/app/_components/RichText/RichText";
 import TableOfContent from "@/app/_components/TableOfContent/TableOfContent";
 import { calculateReadingTime } from "@/app/_utils/calculateReadingTime";
 import { cmsImageLoader } from "@/app/_utils/imageLoader";
-import { GetArticleDocument, GetArticleQuery } from "@/graphql/codegen/graphql";
+import { GetArticleDocument, GetArticleQuery, GetCommentsDocument, GetCommentsQuery } from "@/graphql/codegen/graphql";
 import { useSuspenseQuery } from "@apollo/client";
-import { Badge, Box, Container, Flex, Heading, Strong, Text } from "@radix-ui/themes";
+import { Badge, Box, Container, Flex, Heading, Separator, Strong, Text } from "@radix-ui/themes";
+import moment from "moment";
 import Image from "next/image";
+import { Fragment } from "react";
 
 // Define - type of props
 type TProps = Readonly<{
@@ -16,12 +20,17 @@ type TProps = Readonly<{
 
 // Page - article
 export default function ArticlePage({ params }: TProps) {
-    const { data }: { data: GetArticleQuery } = useSuspenseQuery(GetArticleDocument, { variables: { id: params.id } });
+    const { data: article }: { data: GetArticleQuery } = useSuspenseQuery(GetArticleDocument, {
+        variables: { id: params.id },
+    });
+    const { data: comments }: { data: GetCommentsQuery } = useSuspenseQuery(GetCommentsDocument, {
+        variables: { id: params.id },
+    });
     const { title, description, date, content, thumbnail, author, tags, category } =
-        data.article?.data?.attributes || {};
+        article.article?.data?.attributes || {};
 
     return (
-        <Box>
+        <Box width="100%">
             {/* Container - main */}
             <Container size="2" px="4">
                 {/* Component - heading */}
@@ -82,7 +91,7 @@ export default function ArticlePage({ params }: TProps) {
                     </Flex>
                 </Flex>
             </Container>
-
+                              
             <Container size="2" px="4" className="relative">
                 {/* Components - tocs */}
                 <Box className="absolute top-[20px] left-[20px] h-full">
@@ -106,6 +115,42 @@ export default function ArticlePage({ params }: TProps) {
                     {/* Text - content */}
                     <Flex direction="column" gap="4">
                         <RichText content={content} />
+                    </Flex>
+
+                    <Separator my="3" size="4" />
+
+                    {/* Component - comments */}
+                    <Flex direction="column" gap="5">
+                        {/* Title - commen & total */}
+                        <Heading as="h5" size="5">
+                            {comments.comments?.meta.pagination.total} Comments
+                        </Heading>
+
+                        {/* List - comment */}
+                        {comments.comments?.data.map((comment, idx: number) => (
+                            <Fragment key={idx}>
+                                <Comment
+                                    name={comment.attributes?.name || ""}
+                                    content={comment.attributes?.content}
+                                    createdAt={moment(comment.attributes?.createdAt).format("DD-MM-YYYY")}
+                                />
+                                <Separator my="3" size="4" />
+                            </Fragment>
+                        ))}
+                    </Flex>
+
+                    {/* Component - comment form */}
+                    <Flex direction="column" gap="4">
+                        {/* Title - commen & total */}
+                        <Heading as="h5" size="5">
+                            Your comment
+                        </Heading>
+
+                        {/* Text - note */}
+                        <Text color="gray">Your email will not be published*</Text>
+
+                        {/* Form - comment */}
+                        <CommentForm articleId={article.article?.data?.id || ""} />
                     </Flex>
                 </Flex>
             </Container>
--- frontend/app/articles/page.tsx
+++ frontend/app/articles/page.tsx
@@ -40,7 +40,6 @@ export default function ArticlesPage() {
         if (currentPage > (articles?.meta?.pagination?.pageCount || 1)) {
             observer.disconnect();
         }
-        console.log(observer);
         return () => observer.disconnect();
     }, [isIntersecting]);
 
@@ -58,84 +57,82 @@ export default function ArticlesPage() {
     }, [isIntersecting]);
 
     return (
-        <Box>
-            <Container size="4" px="4">
-                {/* Component - heading */}
-                <Flex direction="column" align="center" justify="center" gap="4" py="8">
-                    {/* Title - title page */}
-                    <Heading as="h1" size="6" className="text-center">
-                        {title}
-                    </Heading>
+        <Container size="4" px="4">
+            {/* Component - heading */}
+            <Flex direction="column" align="center" justify="center" gap="4" py="8">
+                {/* Title - title page */}
+                <Heading as="h1" size="6" className="text-center">
+                    {title}
+                </Heading>
 
-                    {/* Component - description */}
-                    <Box maxWidth="600px" className="text-center">
-                        <Text color="gray">{description}</Text>
-                    </Box>
-                </Flex>
+                {/* Component - description */}
+                <Box maxWidth="600px" className="text-center">
+                    <Text color="gray">{description}</Text>
+                </Box>
+            </Flex>
 
-                {/* List - articles */}
-                <Grid columns="2" gap="4" width="100%" mb="8">
-                    {visibleArticles.length
-                        ? visibleArticles.map((article: any, idx: number) => (
-                              <Card
-                                  key={idx}
-                                  className="text-center"
-                                  onClick={() => router.push(`/articles/${article.id}/${article.attributes?.slug}`)}
-                              >
-                                  {/* Image - thumbnail */}
-                                  <Image
-                                      width={120}
-                                      height={120}
-                                      loader={cmsImageLoader}
-                                      src={article?.attributes?.thumbnail?.data?.attributes?.url || ""}
-                                      alt={article?.attributes?.thumbnail?.data?.attributes?.alternativeText || ""}
-                                      className="w-full mb-4"
-                                  />
+            {/* List - articles */}
+            <Grid columns="2" gap="4" width="100%" mb="8">
+                {visibleArticles.length
+                    ? visibleArticles.map((article: any, idx: number) => (
+                          <Card
+                              key={idx}
+                              className="text-center"
+                              onClick={() => router.push(`/articles/${article.id}/${article.attributes?.slug}`)}
+                          >
+                              {/* Image - thumbnail */}
+                              <Image
+                                  width={120}
+                                  height={120}
+                                  loader={cmsImageLoader}
+                                  src={article?.attributes?.thumbnail?.data?.attributes?.url || ""}
+                                  alt={article?.attributes?.thumbnail?.data?.attributes?.alternativeText || ""}
+                                  className="w-full mb-4"
+                              />
 
-                                  {/* Component - information       */}
-                                  <Flex direction="column" gap="2">
-                                      <Flex gap="2">
-                                          {/* Tag - category */}
-                                          <Badge variant="outline" className="w-fit" mb="2">
-                                              {article?.attributes?.category?.data?.attributes?.title}
-                                          </Badge>
+                              {/* Component - information       */}
+                              <Flex direction="column" gap="2">
+                                  <Flex gap="2">
+                                      {/* Tag - category */}
+                                      <Badge variant="outline" className="w-fit" mb="2">
+                                          {article?.attributes?.category?.data?.attributes?.title}
+                                      </Badge>
 
-                                          {/* Divider - bullet point */}
-                                          <Text>•</Text>
+                                      {/* Divider - bullet point */}
+                                      <Text>•</Text>
 
-                                          {/* Text - time start article */}
-                                          <Text color="gray">{calculateReadingTime(article?.attributes?.content)}</Text>
-                                      </Flex>
-
-                                      {/* Title - title */}
-                                      <Heading as="h6" size="3" mb="1">
-                                          {article?.attributes?.title}
-                                      </Heading>
-
-                                      {/* Text - description */}
-                                      <Text color="gray">{article?.attributes?.description}</Text>
-
-                                      <Flex gap="2">
-                                          {/* Text - author name */}
-                                          <Text>{article?.attributes?.author?.data?.attributes?.username}</Text>
-
-                                          {/* Divider - bullet point */}
-                                          <Text>•</Text>
-
-                                          {/* Text - time start article */}
-                                          <Text color="gray"> {article?.attributes?.date}</Text>
-                                      </Flex>
+                                      {/* Text - time start article */}
+                                      <Text color="gray">{calculateReadingTime(article?.attributes?.content)}</Text>
                                   </Flex>
-                              </Card>
-                          ))
-                        : null}
-                </Grid>
 
-                {/* Text - loading more */}
-                <Text ref={ref} className="text-center">
-                    {currentPage <= (articles?.meta?.pagination?.pageCount || 0) ? "Loading..." : null}
-                </Text>
-            </Container>
-        </Box>
+                                  {/* Title - title */}
+                                  <Heading as="h6" size="3" mb="1">
+                                      {article?.attributes?.title}
+                                  </Heading>
+
+                                  {/* Text - description */}
+                                  <Text color="gray">{article?.attributes?.description}</Text>
+
+                                  <Flex gap="2">
+                                      {/* Text - author name */}
+                                      <Text>{article?.attributes?.author?.data?.attributes?.username}</Text>
+
+                                      {/* Divider - bullet point */}
+                                      <Text>•</Text>
+
+                                      {/* Text - time start article */}
+                                      <Text color="gray"> {article?.attributes?.date}</Text>
+                                  </Flex>
+                              </Flex>
+                          </Card>
+                      ))
+                    : null}
+            </Grid>
+
+            {/* Text - loading more */}
+            <Text ref={ref} className="text-center">
+                {currentPage <= (articles?.meta?.pagination?.pageCount || 0) ? "Loading..." : null}
+            </Text>
+        </Container>
     );
 }
