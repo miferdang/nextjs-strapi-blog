da34b95bb62cc5d1b44bf59e02a9ece99f84ac6c
miferdang <dangphuong19vn@gmail.com>
2024-07-04 11:48:58 +0700

    Update Apollo Client config

--- frontend/apollo/client.apollo.ts
+++ frontend/apollo/client.apollo.ts
@@ -24,9 +24,9 @@ export const makeClient = () => {
     const errorLink = onError(({ graphQLErrors, networkError }) => {
         if (graphQLErrors)
             graphQLErrors.forEach(({ message, locations, path }) =>
-                console.log(`[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`)
+                console.error(`[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`)
             );
-        if (networkError) console.log(`[Network error]: ${networkError}`);
+        if (networkError) console.error(`[Network error]: ${networkError}`);
     });
 
     return new ApolloClient({
