62fc43550bd32df381f2be234ad3323ad0a75c96
miferdang <dangphuong19vn@gmail.com>
2024-07-05 11:51:04 +0700

    Update layout for pages with error

--- frontend/app/_components/Error/PageError.tsx
+++ frontend/app/_components/Error/PageError.tsx
@@ -1,4 +1,4 @@
-import { Box, Container, Flex, Heading, Text } from "@radix-ui/themes";
+import { Flex, Heading, Text } from "@radix-ui/themes";
 import Image from "next/image";
 
 // Define - type of props
@@ -12,23 +12,19 @@ type TProps = Readonly<{
 // Component - error display for page
 const PageError = ({ status, title, imageUrl, extra }: TProps) => {
     return (
-        <Box flexBasis="auto" flexGrow="1" flexShrink="1">
-            <Container size="4">
-                <Flex direction="column" align="center" justify="between" gap="4" my="6">
-                    {/* Title  - error's title */}
-                    <Heading as="h1">{title}</Heading>
+        <Flex direction="column" align="center" justify="center" gap="4" width="100%" height="100%">
+            {/* Title  - error's title */}
+            <Heading as="h1">{title}</Heading>
 
-                    {/* Text - error status code */}
-                    <Text>Error code: {status}</Text>
+            {/* Text - error status code */}
+            <Text>Error code: {status}</Text>
 
-                    {/* Image - error image */}
-                    {imageUrl ? <Image width={400} height={400} src={imageUrl} alt={title} /> : null}
+            {/* Image - error image */}
+            {imageUrl ? <Image width={400} height={400} src={imageUrl} alt={title} /> : null}
 
-                    {/* Component - action */}
-                    {extra}
-                </Flex>
-            </Container>
-        </Box>
+            {/* Component - action */}
+            {extra}
+        </Flex>
     );
 };
 
--- frontend/app/error.tsx
+++ frontend/app/error.tsx
@@ -1,8 +1,9 @@
 "use client";
 
-import { Box, Button } from "@radix-ui/themes";
-import PageError from "./_components/Error/PageError";
+import { Button } from "@radix-ui/themes";
 import { useEffect } from "react";
+import PageError from "./_components/Error/PageError";
+import { ArrowLeftIcon } from "lucide-react";
 
 // Page - 500 error
 export default function Error({ error, reset }: { error: Error & { digest?: string }; reset: () => void }) {
@@ -12,17 +13,15 @@ export default function Error({ error, reset }: { error: Error & { digest?: stri
     }, [error]);
 
     return (
-        <Box>
-            <PageError
-                title="Something went wrong!"
-                status={500}
-                imageUrl=""
-                extra={
-                    <Button variant="outline" highContrast onClick={reset}>
-                        Back to Home Page
-                    </Button>
-                }
-            />
-        </Box>
+        <PageError
+            title="Something went wrong!"
+            status={500}
+            extra={
+                <Button variant="outline" highContrast onClick={reset}>
+                    <ArrowLeftIcon />
+                    Back to Home Page
+                </Button>
+            }
+        />
     );
 }
