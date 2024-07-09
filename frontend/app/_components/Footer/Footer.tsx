a0078ea4422e530be72e70e6e353102f0c8b9719
miferdang <dangphuong19vn@gmail.com>
2024-07-09 11:53:47 +0700

    Update style of navbar and footer

---
+++ frontend/app/_components/Footer/Footer.tsx
@@ -0,0 +1,12 @@
+import { Container, Flex, Text } from "@radix-ui/themes";
+
+// Component - footer
+const Footer = () => {
+    return (
+        <Flex align="center" justify="center" width="100%">
+            <Text>Copyright © 2024 | Little Fox</Text>
+        </Flex>
+    );
+};
+
+export default Footer;
--- frontend/app/_components/Navbar/Navbar.tsx
+++ frontend/app/_components/Navbar/Navbar.tsx
@@ -1,71 +1,116 @@
-import { flattenResponse } from "@/app/_utils/flattenResponse";
 import { cmsImageLoader } from "@/app/_utils/imageLoader";
 import { GetNavigationDocument, GetNavigationQuery } from "@/graphql/codegen/graphql";
 import { useSuspenseQuery } from "@apollo/client";
-import { Box, Button, Container, Flex, Link, TextField } from "@radix-ui/themes";
-import { Search } from "lucide-react";
+import { Box, Button, Card, Flex, Link, TextField } from "@radix-ui/themes";
+import { MenuIcon, Search } from "lucide-react";
 import Image from "next/image";
 import { usePathname, useRouter } from "next/navigation";
+import { useCallback, useState } from "react";
+import Dropdown from "../Dropdown/Dropdown";
 
 // Component - navbar
 const Navbar = () => {
     const router = useRouter();
     const pathname = usePathname();
+    const [keyword, setKeyword] = useState("");
     const { data }: { data: GetNavigationQuery } = useSuspenseQuery(GetNavigationDocument);
     const { logo, menus } = data.navigation?.data?.attributes ?? {};
 
+    // Handle - enter search by pressing enter key down
+    const onKeyDown = useCallback(
+        (e: any) => {
+            if (e.key === "Enter" && keyword) {
+                router.push(`/search?keyword=${encodeURIComponent(keyword)}`);
+            }
+        },
+        [keyword]
+    );
+
+    // Handle - change input search
+    const onSearchChange = useCallback((e: any) => {
+        setKeyword(e.target.value);
+    }, []);
+
     return (
-        <Flex flexBasis="auto" flexGrow="0" flexShrink="1" className="border-b border-gray-300">
-            <Container size="4">
-                <Flex align="center" justify="between">
-                    {/* Component - left side */}
-                    <Flex gap="4">
-                        {/* Component - logo */}
-                        <Box p="4">
-                            <Image
-                                width={40}
-                                height={40}
-                                loader={cmsImageLoader}
-                                src={logo?.data?.attributes?.url || ""}
-                                alt={logo?.data?.attributes?.alternativeText || ""}
-                            />
-                        </Box>
+        <Flex align="center" justify="between" p="4">
+            {/* Component - left side */}
+            <Flex gap={{ sm: "6", initial: "4" }} align="center">
+                {/* Component - logo */}
+                <Box onClick={() => router.push("/")}>
+                    <Image
+                        width={40}
+                        height={40}
+                        loader={cmsImageLoader}
+                        src={logo?.data?.attributes?.url || ""}
+                        alt={logo?.data?.attributes?.alternativeText || ""}
+                    />
+                </Box>
 
-                        {/* Component - menus */}
-                        <Flex align="center" gap="6" p="4">
-                            {menus?.length
-                                ? menus.map((menu, idx: number) => {
-                                      return (
-                                          <Link
-                                              key={idx}
-                                              href={menu?.url || ""}
-                                              color="gray"
-                                              highContrast={pathname === menu?.url ? true : false}
-                                          >
-                                              {menu?.label}
-                                          </Link>
-                                      );
-                                  })
-                                : null}
-                        </Flex>
-                    </Flex>
-
-                    {/* Component - right side */}
-                    <Flex align="center" gap="4" py="2" px="4">
-                        {/* Component - search box */}
-                        <TextField.Root placeholder="Search the content…">
-                            <TextField.Slot>
-                                <Search size={16} />
-                            </TextField.Slot>
-                        </TextField.Root>
-
-                        {/* Button - contact me */}
-                        <Button variant="outline" highContrast onClick={() => router.push("/contact-me")}>
-                            Contact Me
-                        </Button>
-                    </Flex>
+                {/* Component - menus */}
+                <Flex align="center" gap="6" className="md:flex hidden">
+                    {menus?.length
+                        ? menus.map((menu, idx: number) => {
+                              return (
+                                  <Link
+                                      key={idx}
+                                      href={menu?.url || ""}
+                                      color="gray"
+                                      highContrast={pathname === menu?.url ? true : false}
+                                  >
+                                      {menu?.label}
+                                  </Link>
+                              );
+                          })
+                        : null}
                 </Flex>
-            </Container>
+            </Flex>
+
+            {/* Component - right side */}
+            <Flex align="center" gap="4">
+                {/* Component - search box */}
+                <TextField.Root
+                    placeholder="Search articles…"
+                    value={keyword}
+                    onChange={onSearchChange}
+                    onKeyDown={onKeyDown}
+                >
+                    <TextField.Slot>
+                        <Search size={16} />
+                    </TextField.Slot>
+                </TextField.Root>
+
+                {/* Button - contact me */}
+                <Button
+                    variant="outline"
+                    className="md:block hidden"
+                    highContrast
+                    onClick={() => router.push("/contact")}
+                >
+                    Contact Me
+                </Button>
+
+                {/* Component - dropdown menus */}
+                <Dropdown
+                    position="right"
+                    trigger={<MenuIcon size={20} />}
+                    content={
+                        menus?.length
+                            ? menus.map((menu, idx: number) => {
+                                  return (
+                                      <Link
+                                          key={idx}
+                                          href={menu?.url || ""}
+                                          color="gray"
+                                          highContrast={pathname === menu?.url ? true : false}
+                                      >
+                                          {menu?.label}
+                                      </Link>
+                                  );
+                              })
+                            : null
+                    }
+                />
+            </Flex>
         </Flex>
     );
 };
